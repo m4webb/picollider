c1c4ae2ac652a0fb9b44fe40820efdcbca4af32e
Matthew <mawebb@math.byu.edu>
2014-03-20 22:24:38 -0600

    Parameter objects.
    Signed-off-by: Matthew <mawebb@math.byu.edu>

--- flits.py
+++ flits.py
@@ -4,62 +4,80 @@ from threading import Thread
 
 import pisynth
 import brain
-
-def cc(first, second, weight):
-    """Return the convex combination (weight)*first + (1 - weight)*second"""
-    return weight*first + (1 - weight)*second
+import parameters
 
 class FlitterMood(object):
-    def __init__(self, client, message=None):
-        self.client = client
-        self.flitter = Flitter(self.client)
-        if message and message.mood == 'Flitter':
-            for key in message.contents:
-                if key == 'freq':
-                    self.flitter.freq.set(message.contents[key])
-                else:
-                    setattr(self.flitter, key, message.contents[key])
+    def __init__(self, manager):
+        self.manager = manager
+        self.mood_name = 'Flitter'
+        self.running = False
 
     def read_message(self, message):
-        if message.mood != 'Flitter':
+        if message.mood != self.mood_name:
             return
-        weight = message.confidence
-        for key in message.contents:
-            if key in ('freq',):
-                new_val = cc(message.contents[key], self.flitter.freq.get(), weight)
-                self.flitter.freq.set(new_val)
-            elif key in ('overtone_amps, overtone_ds'):
-                list_to_change = getattr(self.flitter, key)
-                message_list = message.contents[key]
-                for i in range(len(list_to_change)):
-                    new_val = cc(message_list[i], list_to_change[i], weight)
-                    list_to_change[i] = new_val
-            elif key in ('wait', 'density', 'decay'):
-                new_val = cc(message.contents[key], getattr(self.flitter, key),
-                             weight) 
-                setattr(self.flitter, key, new_val)
+        probability = message.confidence
+        for parameter in self.flitter.parameters:
+            if random.random() < probability:
+                parameter.respond_to_message(message)
 
     def create_message(self, confidence, influence):
-        pass
+        message = brain.Message(confidence, self.mood_name)
+        probability = influence
+        for parameter in self.flitter.parameters:
+            if random.random() < probability:
+                parameter.prepare_message(message)
 
-    def enter(self):
+    def enter(self, message=None):
+        if self.running:
+            return
+        self.flitter = Flitter(self.manager)
+        if message and message.mood == self.mood_name:
+            for parameter in self.flitter.parameters:
+                parameter.initialize_message(message)
         self.flitter.start()
+        self.running = True
 
     def leave(self):
+        if not self.running:
+            pass
         self.flitter.stop()
+        self.running = False
 
+    def perturb(self, magnitude):
+        if random.random() < magnitude:
+            self.flitter.wait.set(random.choice((0.2, 0.1, 0.08, 0.05)))
+        if random.random() < magnitude:
+            self.flitter.freq.normedval = random.random()
+        if random.random() < magnitude:
+            self.flitter.density.set(random.random())
+        for i in range(len(self.flitter.overtone_amps)):
+            if random.random() < magnitude:
+                self.flitter.overtone_amps[i] = random.choice((0.1, 0.08, 0.06,
+                                                0.04, 0.0, 0.0, 0.0))/(1. + i)
+            if random.random() < magnitude:
+                if random.random() < 0.5:
+                    self.flitter.overtone_ds[i] = 50 - 100*random.random()
+                else:
+                    self.flitter.overtone_ds[i] = 0
+        if random.random() < magnitude:
+            self.flitter.decay.set(random.choice((0.15, 0.1, 0.08, 0.05,
+                                                  0.02)))
        
 class Flitter(Thread):
-    def __init__(self, client):
+    def __init__(self, manager):
         super().__init__()
-        self.client = client
-        self.wait = 0.1
-        self.freq = brain.WalkingParameter(100, 1000, 0.001) 
-        self.density = 0.8
-        self.overtone_amps = [0.1, 0, 0, 0, 0, 0, 0, 0]
-        self.overtone_ds = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.decay = 0.05
+        self.manager = manager
+        self.wait = parameters.ConvexCombinationValue('wait', 0.1)
+        self.freq = parameters.WalkingParameter('freq', 100, 1000, 0.001) 
+        self.density = parameters.ConvexCombinationValue('density', 0.8)
+        self.overtone_amps = parameters.StickyList('overtone_amps', 
+                             [0.1, 0, 0, 0, 0, 0, 0, 0])
+        self.overtone_ds = parameters.ConvexCombinationList('overtone_ds', 
+                           [0, 0, 0, 0, 0, 0, 0, 0])
+        self.decay = parameters.ConvexCombinationValue('decay', 0.05)
         self.running = False
+        self.parameters = [self.wait, self.freq, self.density,
+                           self.overtone_amps, self.overtone_ds, self.decay]
 
     def __repr__(self):
         return "\n".join(["{:20} {}".format(attr, repr(getattr(self, attr))) for 
@@ -69,40 +87,9 @@ class Flitter(Thread):
     def run(self):
         self.running = True
         while self.running:
-
-            """
-            self.dwait += 0.005 - random.random()*0.01
-            if self.dwait < -.1:
-                self.dwait = 0
-            if self.dwait > .1:
-                self.dwait = 0
-            self.wait += self.dwait
-            if self.wait < 0.01:
-                self.wait = 0.01
-                self.dwait = 0.01
-            if self.wait > 0.7:
-                self.wait = 0.7
-                self.dwait = -0.01
-            """
-
-            """
-            self.dfreq += 0.5 - random.random()*1
-            if self.dfreq < -3:
-                self.dfreq = -2
-            if self.dfreq > 3:
-                self.dfreq = 2
-            self.freq += self.dfreq
-            if self.freq < 100:
-                self.freq = 100
-                self.dfreq = 2
-            if self.freq > 1000:
-                self.freq = 1000
-                self.dfreq = -2
-            """
-
-            if random.random() < self.density:
+            if random.random() < self.density.get():
                 self.play_next_flit()
-            time.sleep(self.wait)
+            time.sleep(self.wait.get())
 
     def stop(self):
         self.running = False
@@ -122,9 +109,9 @@ class Flitter(Thread):
         args['env_level4'] = 0
         args['env_time0'] = 0.01
         args['env_time1'] = 0.01
-        args['env_time2'] = self.decay
+        args['env_time2'] = self.decay.get()
         args['env_time3'] = 0
         args['env_releaseNode'] = 2
         args['env_loopNode'] = 1
-        synth = pisynth.PiSynth(self.client, **args)
+        synth = pisynth.PiSynth(self.manager, **args)
         synth.start()
---
+++ parameters.py
@@ -0,0 +1,152 @@
+import random
+
+class Parameter(object):
+    def __init__(self, name):
+        self.name = name
+    def respond_to_message(self, message):
+        raise NotImplementedError
+    def prepare_message(self, message):
+        raise NotImplementedError
+    def initialize_from_message(self, message):
+        raise NotImplementedError
+    def get(self):
+        raise NotImplementedError
+
+class SingleObjectParameter(Parameter):
+    def __init__(self, name, obj):
+        super().__init__(name)
+        self.obj = obj 
+    def prepare_message(self, message):
+        message.contents[self.name] = self.obj
+    def initialize_from_message(self, message):
+        if self.name in message.contents:
+            self.obj = message.contents[self.name]
+    def get(self):
+        return self.obj
+    def set(self, obj):
+        self.obj = obj
+    def __repr__(self):
+        return "<{} '{}' {}>".format(self.__class__.__name__, self.name, 
+                                   self.obj)
+    def __str__(self):
+        return str(self.obj)
+
+class SetParameter(SingleObjectParameter):
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            probability = message.confidence
+            for item in message.contents[self.name] - self.obj:
+                if random.random() < probability:
+                    self.obj.add(item)
+            for item in self.obj - message.contents[self.name]:
+                if random.random() < probability:
+                    self.obj.remove(item)
+
+class ListParameter(SingleObjectParameter):
+    def __getitem__(self, index):
+        return self.obj[index]
+    def __setitem__(self, index, val):
+        self.obj[index] = val
+    def __len__(self):
+        return len(self.obj)
+
+class ConvexCombinationList(ListParameter):
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            weight = message.confidence
+            for i in range(len(self.obj)):
+                self.obj[i] = weight*message.contents[self.name][i] +\
+                              (1. - weight)*self.obj[i]
+
+class StickyList(ListParameter):
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            probability = message.confidence
+            for i in range(len(self.obj)):
+                if random.random() < probability:
+                    self.obj[i] = messaage.contents[self.name][i]
+
+class ConvexCombinationValue(SingleObjectParameter):
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            weight = message.confidence
+            self.obj = weight*message.contents[self.name] +\
+                       (1. - weight)*self.obj
+
+class StickyValue(SingleObjectParameter):
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            probability = message.confidence
+            if random.random() < probability:
+                self.obj = message.contents[self.name]
+
+class WalkingParameter(Parameter):
+    def __init__(self, name, low, high, viscosity):
+        self.name = name
+        self.low = float(low)
+        self.high = float(high)
+        self.viscosity = float(viscosity)
+        self.span = self.high - self.low
+        self.normedval = 0.5
+        self.dx = 0.0
+
+    def __repr__(self):
+        return "<WalkingParameter {}>".format(str(self.get()))
+
+    def __str__(self):
+        return str(self.get())
+
+    def __iadd__(self, val):
+        domainval = self._normed_to_domain(self.normedval) + val
+        self.normedval = self._domain_to_normed(domainval)
+        self._check_bounds()
+        return self
+
+    def __isub__(self, val):
+        domainval = self._normed_to_domain(self.normedval) - val
+        self.normedval = self._domain_to_normed(domainval)
+        self._check_bounds()
+        return self
+
+    def _check_bounds(self):
+        self.normedval = min(1, max(0, self.normedval))
+
+    def _normed_to_domain(self, val):
+        return self.low + val*self.span
+
+    def _domain_to_normed(self, val):
+        return (val - self.low)/self.span
+
+    def respond_to_message(self, message):
+        if self.name in message.contents:
+            weight = message.confidence
+            self.set(weight*message.contents[self.name] +\
+                     (1. - weight)*self.get())
+
+    def prepare_message(self, message):
+        message.contents[self.name] = self.get()
+
+    def initialize_from_message(self, message):
+        if self.name in message.contents:
+            self.set(message.contents[self.name])
+
+    def get(self):
+        return self._normed_to_domain(self.normedval)
+
+    def set(self, val):
+        self.normedval = self._domain_to_normed(val)
+        self._check_bounds()
+
+    def walk(self):
+        self.dx += self.viscosity - random.random()*self.viscosity*2.0
+        if self.dx < -5.0*self.viscosity:
+            self.dx = -5.0*self.viscosity
+        elif self.dx > 5.0*self.viscosity:
+            self.dx = 5.0*self.viscosity
+        self.normedval += self.dx
+        if self.normedval > 1.0:
+            self.normedval = 1.0
+            self.dx = -1.0*self.viscosity
+        elif self.normedval < 0.0:
+            self.normedval = 0.0
+            self.dx = 1.0*self.viscosity
